30bcb8f35b82636be08bff8d5ea82e482cfb9b85
Prajwal G Hondad <yourgithubemail@gmail.com>
2026-03-02 19:47:53 +0530

    Added basic folder with JS and HTML files

---
+++ basic/first.js
@@ -0,0 +1,25 @@
+/*const student= {
+    fullname:"prajwal",
+    age:21,
+    sgpa:8.0,
+    ispass:true,
+};*/
+
+/*const product ={
+    title:"ball pen",
+    rating:4,
+    offer:5,
+    price:270,
+
+};
+console.log(product);*/
+/*data type   
+
+
+const profile ={
+    username:"prajwal_g_hondad",
+    followers:390,
+    following:380,
+    post:15,
+};
+console.log(typeof profile["username"]);*/
