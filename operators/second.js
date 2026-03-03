f430c3ea0f005aa20883ce0d61f5c2cc8b607d6a
Prajwal G Hondad <yourgithubemail@gmail.com>
2026-03-04 01:00:25 +0530

    Added second.js

---
+++ operators/second.js
@@ -0,0 +1,60 @@
+/* AIRTHMATIC OPERATION
+
+let a=5;
+let b=8;
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);  */
+
+/*
+let a=6;
+let b=5;
+console.log("cond1 && cond2=",a<b && a===6);
+*/
+
+/*let age = 25;
+
+if (age >= 18) {
+    console.log("Eligible for voting");
+} else {
+    console.log("Not eligible for voting");
+}*/
+
+/*let mode = "blue";
+let color;
+if (mode === "dark") {
+    color = "black";
+} else {
+    color = "white";
+}
+console.log(color);*/
+
+ /*let num=prompt("enteer the number:");
+ if (num%5==0){
+    console.log(num,"is multiiple with 5");
+ }else {
+    console.log(num,"is  not multiiple with 5");
+ }*/
+
+
+let marks = Number(prompt("Enter the marks:"));
+
+if (marks >= 90 && marks <= 100) {
+    console.log(marks, "score is A");
+} else if (marks >= 80) {
+    console.log(marks, "score is B");
+} else if (marks >= 70) {
+    console.log(marks, "score is C");
+} else if (marks >= 60) {
+    console.log(marks, "score is D");
+} else {
+    console.log(marks, "score is F");
+}
+
+
+
+
+     
+
+
